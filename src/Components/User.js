4b79760ce11a04965d180f798ccffad1b98e9fe8
Gaurav chauhan <gauravchauhan2403@gmail.com>
2021-09-07 20:48:33 +0530

    update

--- src/Components/User.js
+++ src/Components/User.js
@@ -5,10 +5,7 @@ const User = ({item}) => {
   return (
     <div className='user'>
       <img className='img' alt={item.name} src={item.avatar} />
-      <div className='description'>
-        <p>{item.lorem}</p>
-        <p>{item.message}</p>
-      </div>
+      <p className='description'>{item.message}</p>
       <h3 className="name">{item.name}</h3>
       <h5>{item.designation}</h5>
       <h5>{item.location}</h5>
